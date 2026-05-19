97d8f273f3970047e0918cf6441458422226b2d5
Jar <thesolesab@gmail.com>
2026-05-19 16:26:31 +0300

    fix: hide profile button if unauthorized

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,13 +7,14 @@ import { useTelegram } from '@/components/providers/TelegramProvider';
 import { TeamList } from '@/components/TeamList';
 import { useUsers } from '@/hooks/useUsers';
 import { useTeams } from '@/hooks/useTeams';
-import { log } from 'console';
+import { useProfile } from '@/hooks/useProfile';
 
 function Dashboard() {
   const router = useRouter();
   const searchParams = useSearchParams();
   const { isReady, startParam, initData, chat } = useTelegram();
   const { registerUser } = useUsers();
+  const {profile} = useProfile();
   const { teams, isLoading, error, joinTeam } = useTeams();
 
   useEffect(() => {
@@ -61,20 +62,17 @@ function Dashboard() {
     );
   }
 
-  console.log(isReady, startParam, initData, chat );
-  
-
   return (
     <div className="flex flex-col items-center justify-start min-h-screen p-4 bg-zinc-50 dark:bg-black text-black dark:text-white">
       <div className="max-w-md w-full space-y-6">
         <div className="flex justify-between items-center bg-white dark:bg-zinc-900 p-4 rounded-2xl border border-zinc-200 dark:border-zinc-800 shadow-sm">
           <h1 className="text-xl font-bold">Мои команды</h1>
-          <button
+          {profile && <button
             onClick={() => router.push('/profile')}
             className="text-sm bg-zinc-100 dark:bg-zinc-800 px-3 py-1.5 rounded-lg font-medium hover:bg-zinc-200 dark:hover:bg-zinc-700 transition-colors"
           >
             👤 Профиль
-          </button>
+          </button>}
         </div>
         <TeamList teams={teams} isLoading={isLoading} error={error} />
       </div>
